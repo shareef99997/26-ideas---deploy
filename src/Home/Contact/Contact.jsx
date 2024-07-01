8b38cc00019f509fae7c26f4e7dd8b79366fe77a
Your Nameemail <shareef.99997@gmail.com>
2024-07-01 15:14:02 +0300

    edit contact p

--- src/Home/Contact/Contact.jsx
+++ src/Home/Contact/Contact.jsx
@@ -6,7 +6,7 @@ function Contact() {
     <div className="Contact" id="Contact" >
       <div className='Contact-Body'>
         <div className="content-wrapper">
-          <h2 className="contact-heading"> لبدأ مشروعك الآن او للإستفسارات </h2>
+          <h2 className="contact-heading"> للشراكة أو للإستفسارات </h2>
           <a className='link' href="/contact-us" >
             <button className="contact-button">
               <i className="fas fa-phone" lang='en'></i>  إتصل بنا
--- src/Pages-en/Home-en/Contact-en/Contact-en.jsx
+++ src/Pages-en/Home-en/Contact-en/Contact-en.jsx
@@ -6,7 +6,7 @@ function Contact_en() {
     <div className="Contact-en" id="Contact">
       <div className='Contact-Body'>
         <div className="content-wrapper">
-          <h2 className="contact-heading">To start your project now or for inquiries</h2>
+          <h2 className="contact-heading">To become a partner or for inquiries</h2>
           <a className='link' href="/en/contact-us">
             <button className="contact-button">
               <i className="fas fa-phone"></i> Contact Us
